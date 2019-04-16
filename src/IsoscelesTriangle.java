36f012102d99ca1ca90ac4b508523060286c784f
Jayson <jayson.k.lai-1@ou.edu>
2019-04-15 23:30:21 -0500

    IsocelesTriangle updated

--- src/IsoscelesTriangle.java
+++ src/IsoscelesTriangle.java
@@ -1,4 +1,12 @@
+import java.awt.Color;
+import java.awt.Point;
 
-public class IsoscelesTriangle {
-
+public class IsoscelesTriangle extends Triangle {
+	public IsocelesTriangle(Point pointLB, int base, int height, Color color, boolean fill) {
+			super(pointLB, color, fill);
+			
+			location[1] = new Point(pointLB.x + base, pointLB.y);
+			location[2] = new Point(pointLB.x + height, pointLB.y);
+	}
+	
 }
